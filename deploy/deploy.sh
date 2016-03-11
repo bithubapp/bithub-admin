756428aa3f8eea2f88905ead4c9493816a916b4d
Mark Stahl <mjstahl@gmail.com>
2016-03-11 17:33:34 -0500

    Add deploy configuration to travis.yml

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -4,7 +4,7 @@ eval "$(ssh-agent -s)"
 chmod 600 deploy-key.pem
 ssh-add deploy-key.pem
 
-git remote add deploy ssh://bithub@45.79.201.206/home/bithub/client.git
+git remote add deploy ssh://bithub@develop.bithub.com/home/bithub/client.git
 # Do not change this line. Travis has the ability push back to the repository
 # https://docs.travis-ci.com/user/github-oauth-scopes/
 git push -f deploy develop:develop
