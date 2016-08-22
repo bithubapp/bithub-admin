753934cf14413fe2540fc4998812ec31b6c53828
Juan Orozco <hi@juanorozco.com>
2016-08-22 11:03:46 -0500

    Add canjs 3.0 modules. Add donejs mocha.

--- src/test/functional.js
+++ src/test/functional.js
@@ -1,15 +1,15 @@
-/* eslint babel/new-cap: 0 */
 import F from 'funcunit';
-import QUnit from 'steal-qunit';
+import mocha from 'steal-mocha';
 
-F.attach(QUnit);
+F.attach(mocha);
 
-QUnit.module('bithub-admin functional smoke test', {
-	beforeEach() {
-		F.open('../development.html');
-	}
+describe('bithub-admin functional smoke test', function(){
+  beforeEach(function(){
+    F.open('../development.html');
+  });
+
+  it('bithub-admin main page shows up', function(){
+    F('title').text('bithub-admin', 'Title is set');
+  });
 });
 
-QUnit.test('bithub-admin main page shows up', function () {
-	F('title').text('bithub-admin', 'Title is set');
-});
