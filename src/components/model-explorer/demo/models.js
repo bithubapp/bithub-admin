9a6f99f64a3a1d3aae329da28e7f9a5653d1a07e
Juan Orozco <hi@juanorozco.com>
2016-08-23 09:41:32 -0500

    Create model explorer to help debug models
    - Model explorer code and demo.
    - Use model explorer by accessing its demo page.
    - Add new models to the models.js file in the demo folder

---
+++ src/components/model-explorer/demo/models.js
@@ -0,0 +1,21 @@
+import Organization from 'bithub-admin/models/organization';
+
+var models = {};
+
+export default models;
+
+models['organization'] = {
+    label: 'Organization',
+    methods: [
+        {value: 'getList', label: 'Get List'},
+		{value: 'get', label: 'Get (one)'},
+        {
+            value:'invite',
+            label: 'Invite',
+            defaultRequest: {
+                email: ''
+            }
+        }
+    ],
+    Model: Organization
+};
