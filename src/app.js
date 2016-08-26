5940d9bbefd9a55244ee97e4e11f1bfc4689e5bb
Mark Stahl <mjstahl@gmail.com>
2016-08-26 11:20:12 -0500

    Comment out fixture imports

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 import route from 'can-route';
 import 'can-route-pushstate';
 import AppViewModel from 'bithub-admin/models/appstate/';
-import debugUtils from 'bithub-admin/utils/debug-util/';
+// import debugUtils from 'bithub-admin/utils/debug-util/';
 
-import 'bithub-admin/models/fixtures/';
+// import 'bithub-admin/models/fixtures/';
 // TODO Do not call this on production or with build.
-debugUtils();
+// debugUtils();
 
 route(':page', {page: 'home'});
 route(':page/:slug', {slug: null});
